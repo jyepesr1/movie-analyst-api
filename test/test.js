42cc100684895b573155c46c0cb2331fcb9025e2
Johan Sebastian Yepes Rios <johanyepes@hotmail.com>
2019-10-04 08:37:06 -0500

    Saving artifact Circle CI

--- test/test.js
+++ test/test.js
@@ -9,19 +9,15 @@ let should = chai.should();
 chai.use(chaiHttp);
 //Our parent block
 describe('API', () => {
-
-/*
-  * Test the /GET route
-  */
+  // Test the /GET route
   describe('/GET home', () => {
-      it('it should GET any reply', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-              done();
-            });
+    it('it should GET any reply', (done) => {
+      chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
       });
+    });
   });
-
 });

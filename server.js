8672d4c4f6b0fe8657ec95e3a302213364b0732e
Johan Sebastian Yepes Rios <johanyepes@hotmail.com>
2019-10-25 07:01:37 -0500

    Removing some comments

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ var pool = mysql.createPool({
 function getMovies(callback) {    
   pool.query("SELECT * FROM movie_db.moviereview",
     function (err, rows) {
-      console.log(rows)
       callback(err, rows); 
     }
   );    
@@ -53,20 +52,6 @@ app.get('/', function(req, res){
 })
 
 // Implement the movies API endpoint
-/* app.get('/movies', function(req, res){
-  var movies = [
-    {title : 'Suicide Squad', release: '2016', score: 8, reviewer: 'Robert Smith', publication : 'The Daily Reviewer'},    
-    {title : 'Batman vs. Superman', release : '2016', score: 6, reviewer: 'Chris Harris', publication : 'International Movie Critic'},
-    {title : 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication : 'MoviesNow'},
-    {title : 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication : 'MyNextReview'},
-    {title : 'Avengers: Age of Ultron', release : '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games'},
-    {title : 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
-    {title : 'Guardians of the Galaxy', release : '2014', score: 10, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'},
-  ]
-
-  res.json(movies);
-})
- */
 app.get('/movies', function(req, res, next) {   
   //now you can call the get-driver, passing a callback function
   getMovies(function (err, moviesResult){ 
